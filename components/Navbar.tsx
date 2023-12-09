b8137c2548c6ff703ee8985f7aab23685c1472eb
Walid Wazini <wazini25@gmail.com>
2023-12-09 17:30:48 +0800

    navbar ui pt.1

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,6 +1,8 @@
 import Link from 'next/link'
 import React from 'react'
 import { getServerSession } from 'next-auth'
+import { FaBell, FaShoppingCart } from "react-icons/fa";
+import { MdShoppingCart } from "react-icons/md";
 
 import SignoutBtn from './SignoutBtn'
 
@@ -11,31 +13,57 @@ const Navbar = async () => {
   // console.log(session?.user)
 
   return (
-    <div className='w-full h-20 bg-red-800 flex justify-center items-center gap-1 ' >
-      <div className='basis-4/5 flex justify-center items-center gap-3 text-sm' >
-        <div className='hover:underline text-lg ' >
-          <Link href={'/'} >Home</Link>
+    <div className='w-full h-36 bg-red-800 flex flex-col justify-center items-center gap-1 ' >
+      <div className='basis-1/4 bg-blue-600 w-full flex justify-between items-center' >
+        <div>Other Links</div>
+        {/* TODO will be turn into dropdown link */}
+        <div  >
+          <div className='flex' >
+            <FaBell />
+            <p className='text-xs' >Notification</p>
+          </div>
         </div>
-        {session?.user
-          ? (<div className='hover:underline text-lg ' >
-            <Link href={'/profile'} >Profile</Link>
-          </div>)
-          : (<>
-            <div className='hover:underline text-lg ' >
-              <Link href={'/signup'} >Signup</Link>
-            </div>
-            <div className='hover:underline text-lg ' >
-              <Link href={'/sign-in'} >Login</Link>
-            </div>
-          </>)}
       </div>
-      <div className='basis-1/5' >
-        {session?.user && (
-          <SignoutBtn />
-        )}
+      <div className='basis-3/4 w-full h-full flex justify-center items-center gap-3 text-sm' >
+        <div className="basis-1/6  h-full flex items-center justify-center ">
+          <Link href={'/home'} className='text-4xl italic font-bold' >
+            Shoplette
+          </Link>
+        </div>
+        <div className="basis-1/2  h-full flex flex-col">
+          <input
+            className='w-full mt-4 p-4 bg-slate-700 ring-0 outline-0 '
+            placeholder='Search anything here..'
+          />
+          <div className='flex mt-2 justify-start items-center gap-3 px-2 text-md' >
+            <span>tag</span>
+            <span>tag</span>
+          </div>
+        </div>
+        <div className="basis-1/6 flex items-center justify-center ">
+          <FaShoppingCart className="h-12 w-12 " />
+          {/* <MdShoppingCart /> */}
+        </div>
       </div>
     </div>
   )
 }
-
 export default Navbar
+
+// {
+//   session?.user
+//   ? (<div className='hover:underline text-lg ' >
+//     <Link href={'/profile'} >Profile</Link>
+//   </div>)
+//   : (<>
+//     <div className='hover:underline text-lg ' >
+//       <Link href={'/signup'} >Signup</Link>
+//     </div>
+//     <div className='hover:underline text-lg ' >
+//       <Link href={'/sign-in'} >Login</Link>
+//     </div>
+//     {session?.user && (
+//       <SignoutBtn />
+//     )}
+//   </>)
+// }
